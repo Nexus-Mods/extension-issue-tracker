63a8248c137cf9008ee6ff92e1a79ab5196210c0
TanninOne <sherb@gmx.net>
2018-07-05 11:33:59 +0200

    don't show issue tracker if user isn't logged in

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import Issues from './Issues';
 import persistentReducer from './reducers';
 
 import * as path from 'path';
-import { types } from 'vortex-api';
+import { types, util } from 'vortex-api';
 
 function main(context: types.IExtensionContext) {
-  context.registerDashlet('Issues', 1, 2, 200, Issues, () => true,
+  context.registerDashlet('Issues', 1, 2, 200, Issues,
+    (state: types.IState) =>
+      util.getSafe(state, ['confidential', 'account', 'nexus', 'APIKey'], undefined) !== undefined,
   () => ({}), { closable: true });
 
   context.registerReducer(['persistent', 'issues'], persistentReducer);
